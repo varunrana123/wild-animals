fd909d643d89a7d3603c30d18236338776d298b4
varun rana <71178772+varunrana123@users.noreply.github.com>
2021-07-15 10:43:42 +0530

    Add files via upload

---
+++ javascript/menu.js
@@ -0,0 +1,16 @@
+// check the menu button:
+function validation()
+{
+const inputName= document.querySelector('#inputName');
+const inputEmail= document.querySelector('#inputEmail');
+const inputMessage= document.querySelector('#inputMessage');
+
+
+    alert('form successfully completed')
+    inputName.value="";
+    inputEmail.value="";
+    inputMessage.value="";
+    return true;
+}
+
+
